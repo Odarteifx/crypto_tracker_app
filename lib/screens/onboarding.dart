2854b1fea4840fbf59a1a45c761f359589f01272
Odarteifx <williamlamp512@gmail.com>
2025-02-25 23:43:10 +0000

    Working on onboarding screen

---
+++ lib/screens/onboarding.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CryptoOnboarding extends StatefulWidget {
+  const CryptoOnboarding({super.key});
+
+  @override
+  State<CryptoOnboarding> createState() => _CryptoOnboardingState();
+}
+
+class _CryptoOnboardingState extends State<CryptoOnboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF1a73e8),
+    );
+  }
+}
