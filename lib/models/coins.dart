53e5513c32bd96ad1d8b9201a9232e849da757d5
Odarteifx <williamlamp512@gmail.com>
2025-03-03 23:25:49 +0000

    Adding models

---
+++ lib/models/coins.dart
@@ -0,0 +1,13 @@
+class Coin {
+  final String name;
+  final String symbol;
+  final double price;
+  final double percentageChange;
+
+  Coin({
+    required this.name,
+    required this.symbol,
+    required this.price,
+    required this.percentageChange,
+  });
+}
